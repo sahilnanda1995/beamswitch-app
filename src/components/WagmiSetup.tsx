a0849f12ca8b499c318344b5802a2df88461edaf
Sahil <sahilnanda1995@gmail.com>
2023-07-21 17:59:59 +0530

    first commit: evm wallet connection

---
+++ src/components/WagmiSetup.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { WagmiConfig, createConfig, configureChains, mainnet } from "wagmi";
+
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { moonbaseAlpha, moonbeam, moonriver } from "viem/chains";
+// import { FaceSmileIcon } from "@heroicons/react/24/outline";
+
+// Configure chains & providers with the Alchemy provider.
+// Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
+const { chains, publicClient } = configureChains(
+  [mainnet, moonbaseAlpha, moonbeam, moonriver],
+  [publicProvider()]
+);
+
+// Set up wagmi config
+const config = createConfig({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: "Injected",
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  publicClient,
+});
+
+export function WagmiSetup({ children }: { children: React.ReactNode }) {
+  return <WagmiConfig config={config}>{children}</WagmiConfig>;
+}
