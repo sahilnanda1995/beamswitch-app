6690d2844d8d7ea601550a69db27f5ab7d6e5f12
Sahil <sahilnanda1995@gmail.com>
2023-07-22 17:38:34 +0530

    wallet connect for dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,54 @@
 "use client";
+import {
+  substrateWalletConnectedAtom,
+  substrateAccountsAtom,
+  substrateSelectedAccountAtom,
+} from "@/atoms";
 import ListBoxNetwork from "@/components/ListBoxNetwork";
 import ListBoxToken from "@/components/ListBoxToken";
+import { web3Accounts, web3Enable } from "@polkadot/extension-dapp";
+import { useAtom } from "jotai";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { useAccount, useConnect } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
 
 export default function Home() {
   const [hasMounted, setHasMounted] = useState(false);
+  const [substrateWalletConnected, setSubstrateWalletConnected] = useAtom(
+    substrateWalletConnectedAtom
+  );
+  const [substrateAccounts, setSubstrateAccounts] = useAtom(
+    substrateAccountsAtom
+  );
+  const [substrateSelectedAccount, setSubstrateSelectedAccount] = useAtom(
+    substrateSelectedAccountAtom
+  );
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  });
+
+  //   const allInjected = await web3Enable("beamswitch");
+  const connectSubWallet = async () => {
+    const allInjected = await web3Enable("beamswitch");
+    console.log("all injected");
+    console.log(allInjected);
+    const allAccounts = await web3Accounts();
+    if (allAccounts.length > 0) {
+      setSubstrateWalletConnected(true);
+      setSubstrateAccounts(allAccounts);
+      setSubstrateSelectedAccount(allAccounts[0].address.toString());
+      console.log(allAccounts[0].address.toString());
+    } else {
+      setSubstrateWalletConnected(false);
+      setSubstrateAccounts([]);
+      setSubstrateSelectedAccount("");
+    }
+  };
+
+  const handleOnClick = async () => {};
+
+  const { address, connector: isConnected } = useAccount();
 
   useEffect(() => setHasMounted(true), []);
 
@@ -35,8 +78,25 @@ export default function Home() {
           />
           <ListBoxToken title="token" />
         </div>
-        <button className="flex w-full flex-row justify-center items-center py-2 px-4 border rounded-lg">
-          connect wallet
+        <button
+          className="flex w-full flex-row justify-center items-center py-2 px-4 border rounded-lg"
+          onClick={() => {
+            if (isConnected && address) {
+              if (substrateWalletConnected && substrateSelectedAccount) {
+                handleOnClick();
+              } else {
+                connectSubWallet();
+              }
+            } else {
+              connect();
+            }
+          }}
+        >
+          {isConnected && address
+            ? substrateWalletConnected && substrateSelectedAccount
+              ? "beam it up"
+              : "connect your polkadot wallet"
+            : "Connect EVM wallet"}
         </button>
       </div>
     </main>
